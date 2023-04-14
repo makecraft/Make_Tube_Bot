7cf928786ee215d6474faf3836a871577f25227c
Makecraft <makeshiro@outlook.com>
2023-04-14 12:46:18 -0400

    black

--- Make_Tube_Bot.py
+++ Make_Tube_Bot.py
@@ -6,21 +6,23 @@ import yt_dlp
 
 app = Client("Make_Tube_Bot")
 
-#Hook que imprime un mensaje cuando yt-dlp envia el estado de finished
-def my_hook(d):
-    if d['status'] == 'finished':
-        print('Done downloading, now converting ...')
 
-#ydl_opts opciones de descarga
+# Hook que imprime un mensaje cuando yt-dlp envia el estado de finished
+def my_hook(d):
+    if d["status"] == "finished":
+        print("Done downloading, now converting ...")
+
+
+# ydl_opts opciones de descarga
 ydl_opts = {
-    'format': 'bestaudio/worst',       
-    'outtmpl': '%(id)s',        
-    'noplaylist' : True,        
-    'progress_hooks': [my_hook],  
+    "format": "bestaudio/worst",
+    "outtmpl": "%(id)s",
+    "noplaylist": True,
+    "progress_hooks": [my_hook],
 }
 
-#Código para descargar video previamente definido en una variable o enlace
+# Código para descargar video previamente definido en una variable o enlace
 with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-    ydl.download([''])
+    ydl.download([""])
 
 app.run()
